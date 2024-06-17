c2b5757e8cd0b17eac9cba5b54addaa01227363b
Thomas Sheehan <tsheehan@indeed.com>
2024-06-17 17:01:04 +0900

    Initial Commit and some updates from React Router learning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import NoMatch from "./NoMatch";
+import UserProfile from "./UserProfile";
+
+function Home() {
+  return <p>Home</p>;
+}
+
+function About() {
+  return <p>About</p>;
+}
 
 function App() {
+  const userId = [1, 2, 3, 4, 5];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <br />
+        <h1>Navbar</h1>
+        {userId.map((id) => (
+          <div key={id}>
+            <Link to={`/user/${id}`}>User {id}</Link>
+          </div>
+        ))}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/user/:userId" element={<UserProfile />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/UserProfile.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function UserProfile() {
+  const [user, setUser] = useState({});
+  const { userId } = useParams();
+
+  // Use `useParams()` and `useEffect()`
+  // Load profile data from https://jsonplaceholder.typicode.com/users/${userId}
+
+  useEffect(() => {
+    async function loadUser() {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${userId}`
+      );
+      const userFromAPI = await response.json();
+      setUser(userFromAPI);
+    }
+    loadUser();
+  }, [userId]);
+
+  // No need to change anything below here
+  if (user.id) {
+    return Object.entries(user).map(([key, value]) => (
+      <div key={key}>
+        <label>{key}</label>: {JSON.stringify(value)}
+        <hr />
+      </div>
+    ));
+  }
+  return "Loading...";
+}
+
+export default UserProfile;
